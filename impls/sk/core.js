d274c7897daea6c4ef05209a2732d47e2d5b09bc
santhosh kumar <58027436+bsanthoshkumar@users.noreply.github.com>
2021-08-30 10:33:26 +0530

    Implement step6_file(read_string, eval, slrup. load-file) in sk

--- impls/sk/core.js
+++ impls/sk/core.js
@@ -1,4 +1,6 @@
+const { readFileSync } = require("fs");
 const Env = require("./env");
+const { read_str } = require("./reader");
 const { MalSymbol, pr_str, Nil, List, Str, isEqual } = require("./types");
 
 const env = new Env(null);
@@ -18,6 +20,12 @@ env.set(new MalSymbol("<"), (...args) => args.reduce((a, b) => a < b));
 env.set(new MalSymbol("<="), (...args) => args.reduce((a, b) => a <= b));
 env.set(new MalSymbol(">"), (...args) => args.reduce((a, b) => a > b));
 env.set(new MalSymbol(">="), (...args) => args.reduce((a, b) => a >= b));
+
+env.set(new MalSymbol("list"), (...values) => new List(values));
+env.set(new MalSymbol("list?"), (list) => list instanceof List);
+env.set(new MalSymbol("pi"), Math.PI);
+env.set(new MalSymbol("empty?"), (x) => x.isEmpty());
+env.set(new MalSymbol("count"), (x) => x.count());
 
 env.set(
   new MalSymbol("pr-str"),
@@ -39,10 +47,17 @@ env.set(new MalSymbol("println"), (...values) => {
   return Nil;
 });
 
-env.set(new MalSymbol("list"), (...values) => new List(values));
-env.set(new MalSymbol("list?"), (list) => list instanceof List);
-env.set(new MalSymbol("pi"), Math.PI);
-env.set(new MalSymbol("empty?"), (x) => x.isEmpty());
-env.set(new MalSymbol("count"), (x) => x.count());
+env.set(new MalSymbol("read-string"), (ast) => {
+  if (ast instanceof Str) return read_str(ast.pr_str());
+  throw new Error(`${pr_str(ast)} is not String`);
+});
 
+env.set(new MalSymbol("slurp"), (ast) => {
+  const filePath = pr_str(ast);
+  try {
+    return readFileSync(filePath, "utf-8");
+  } catch (e) {
+    throw `File ${filePath} ${e} not found`;
+  }
+});
 module.exports = env;
